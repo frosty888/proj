7e2bbcd0ff9d8653cfce55eebb8e4273f231952f
frosty888 <74044264+frosty888@users.noreply.github.com>
2020-11-06 09:28:36 +0100

    Add files via upload

---
+++ fragment (1).c
@@ -0,0 +1,44 @@
+#include <stdio.h>#include <ctype.h>#include <string.h>
+#include <stdlib.h>
+void func1(){	
+char buffer[1024];
+printf("Please enter your user id :");
+fgets(buffer, 1024, stdin);
+if (!isalpha(buffer[0]))     {   
+char errormsg[1044]; 
+strncpy(errormsg, buffer,1024);   
+strcat(errormsg, " is not  a valid ID"); 	}
+}
+
+
+/* f2d and f3d are file descriptors obtained after opening files*/
+void func2(int f2d) {   
+char *buf2;	
+size_t len;    
+read(f2d, &len, sizeof(len));   
+buf = malloc(len+1);   
+read(f2d, buf2, len);  
+buf2[len] = '\0';} 
+
+void func3(int f3d){   
+char *buf3;
+int i, len;
+read(f3d, &len, sizeof(len));
+if (len > 8000) { 
+error("too long"); 
+return; 
+}      
+buf3 = malloc(len);     
+read(f3d, buf3,len);         
+}
+void main()
+{
+char *boo = "boooooooooooooooooooooooooooooooooooooooooooooo";
+char *buffer = (char *)malloc(10 * sizeof(char));
+strcpy(buffer, boo);
+func1();
+func3(len(*boo));
+FILE *aFile = fopen("/tmp/tmpfile", "w");
+fprintf(aFile, "%s", "hello world")
+fclose(aFile);
+}
